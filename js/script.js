b3621f6ab77273d710a9530018486ec750240313
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-11-18 14:56:39 +0100

    logica base

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Creiamo una mini app di todolist con html+css+js in Vue
+//Features principali:
+//un input permette all’utente di scrivere una “cosa da fare”
+//un bottone permette di aggiungere quella "cosa" alla lista in pagina, come promemoria
+//grazie ad un bottone (X) può eliminare quella determinata "cosa" dalla lista
+
+var app = new Vue({
+
+  el: "#app",
+  data: {
+    todo: "",
+    listTodo: []
+  },
+
+  methods: {
+
+    addTodo: function() {
+      if (this.todo != "") {
+        this.listTodo.push(this.todo)
+        this.todo = ""
+      }
+    }
+  }
+});
