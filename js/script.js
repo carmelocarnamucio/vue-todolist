ba62d7bbe39f117a26d749e9830923c62dbf7896
carmelocarnamucio <carnamuciocarmelo@gmail.com>
2020-11-18 23:20:59 +0100

    fix transizioni e aggiunto alert doppione

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 //grazie ad un bottone (X) può eliminare quella determinata "cosa" dalla lista
 //BONUS:
 //dò la possibilità anche di inserire la nuova “cosa da fare”, scritta nell’input anche con la pressione del tasto ‘enter’ da tastiera;
-// aggiunto tasto cancella tutto
+//aggiunto tasto cancella tutto
+//aggiunta transizione quando viene eliminato elemento
+//alert che ti avverte che hai già inserito lo stesso elemento nella lista
 
 var app = new Vue({
 
@@ -18,7 +20,10 @@ var app = new Vue({
   methods: {
 
     addTodo: function() {
-      if (this.todo != "") {
+      if (this.listTodo.includes(this.todo)) {
+        alert("PROMEMORIA: Hai già inserito questa nota nella lista");
+        this.listTodo.push(this.todo)
+      } else {
         this.listTodo.push(this.todo)
         this.todo = ""
       }
